969af2bc81c3aeab37c099dcf37c0a1534e9d061
Антон Кротов <toxa.krotoff@gmail.com>
2026-03-14 10:49:20 +0300

    preloader

--- nextjs-project/drift.sql
+++
@@ -1,4 +0,0 @@
-[dotenv@17.3.1] injecting env (0) from .env -- tip: ⚙️  load multiple .env files with { path: ['.env.local', '.env'] }
-[dotenv@17.3.1] injecting env (21) from .env.local -- tip: ⚙️  enable debug logging with { debug: true }
--- This is an empty migration.
-
